53275289a588c7cc535bdaf55c6eb009c9506c1f
Celia Negrin <villarruelistasanluis@gmail.com>
2025-03-26 18:13:27 -0300

    a ver gaston

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,40 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('mi-juego-pwa-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'game.js',
-          'manifest.json',
-          'icono.png',
-          'icono-512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
+const CACHE_NAME = "noticias-cache-v1";
+const urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
+      fetch(event.request).then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(() => caches.match(event.request))
     );
   });
   
